93768263ca96f7f517698bc0243ccecaaf8f5469
Joel Jimenez <Joel.jimenez@solvex.com.do>
2024-09-25 19:32:03 -0400

    edit jofreilyn

--- src/app/pages/question/question.component.ts
+++ src/app/pages/question/question.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-question',
   templateUrl: './question.component.html',
   styleUrl: './question.component.css'
 })
-export class QuestionComponent {
+export class QuestionComponent implements OnDestroy {
+
+  percentage: number = 100;
+  interval: any;
+
+  constructor(private router: Router){
+    this.doCount()
+  }
+
+  doCount(){
+    this.interval = setInterval(() => {
+      this.percentage -= 10;
+
+      if(this.percentage == 0){
+        clearInterval(this.interval);
+        this.router.navigate(['/select'])
+      }
+    }, 1000);
+  }
+
+  ngOnDestroy(): void {
+    if(this.interval){
+      clearInterval(this.interval);
+    }
+  }
+
 
 }
