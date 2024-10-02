864b3d2619739927b79bbfc688770194c4de67cc
Jofreylin Perez Valdez <jperezv@isf.do>
2024-10-02 12:09:48 -0400

    added music to home, lost woods

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit, OnDestroy {
 
+  private audio: HTMLAudioElement = new Audio();  // Inicializamos el objeto de Audio
+
+  ngOnInit() {
+    this.playBackgroundMusic();  // Iniciar música
+  }
+
+
+  playBackgroundMusic(): void {
+    this.audio.src = 'musica/soundtrack.mp3'; // Verifica que la ruta sea correcta
+    this.audio.loop = true; // La música se repite
+    this.audio.volume = 0.2;
+    this.audio.play().catch(error => {
+      console.error('Error al reproducir la música:', error);
+    });
+  }
+
+  ngOnDestroy(): void {
+    // Detener la música al salir del componente
+    if (this.audio) {
+      this.audio.pause();
+      this.audio.currentTime = 0;
+    }
+  }
 }
